2d098971ee7febc0b6cf9f082e0027618b8f86b9
Connor Peet <connor@peet.io>
2021-05-06 12:26:04 -0700

    update to test adapter changes

--- src/extension.ts
+++ src/extension.ts
@@ -6,22 +6,26 @@ import * as vscode from 'vscode';
 import { testExplorerExtensionId, TestHub } from 'vscode-test-adapter-api';
 import { TestControllerFactory } from './testControllerFactory';
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-  const testExplorerExtension = vscode.extensions.getExtension<TestHub>(testExplorerExtensionId);
-  if (!testExplorerExtension) {
-    return;
-  }
+  context.subscriptions.push(
+    vscode.commands.registerCommand('testExplorerConverter.activate', () => {
+      const testExplorerExtension = vscode.extensions.getExtension<TestHub>(
+        testExplorerExtensionId
+      );
+      if (!testExplorerExtension) {
+        return;
+      }
 
-  const testHub = testExplorerExtension.exports;
-  const factory = new TestControllerFactory();
-  context.subscriptions.push(factory);
+      const testHub = testExplorerExtension.exports;
+      const factory = new TestControllerFactory();
+      context.subscriptions.push(factory);
 
-  testHub.registerTestController(factory);
-  context.subscriptions.push({
-    dispose() {
-      testHub.unregisterTestController(factory);
-    },
-  });
+      testHub.registerTestController(factory);
+      context.subscriptions.push({
+        dispose() {
+          testHub.unregisterTestController(factory);
+        },
+      });
+    })
+  );
 }

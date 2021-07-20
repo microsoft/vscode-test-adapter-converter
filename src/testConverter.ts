b8ba873e6c2419e4783e0602cbeb953efe723f21
Connor Peet <connor@peet.io>
2021-07-19 17:11:24 -0700

    fix: remove no-op extra setting prompt

--- src/testConverter.ts
+++ src/testConverter.ts
@@ -122,7 +122,6 @@ export class TestConverter implements vscode.Disposable {
   private syncTopLevel(suite: TestSuiteInfo) {
     const ctrl = this.acquireController(suite.label);
     this.syncItemChildren(ctrl.items, suite.children);
-    promptDisableExplorerUi(); // prompt the first time we discover tests
   }
 
   /**
@@ -252,40 +251,4 @@ const convertedStates = {
   skipped: vscode.TestResultState.Skipped,
   errored: vscode.TestResultState.Errored,
   completed: vscode.TestResultState.Unset,
-};
-
-const settings = [
-  'testExplorer.gutterDecoration',
-  'testExplorer.codeLens',
-  'testExplorer.errorDecoration',
-  'testExplorer.errorDecorationHover',
-];
-
-let shouldPromptForUiDisable = vscode.workspace.getConfiguration().get(settings[0]) !== false;
-
-const promptDisableExplorerUi = async () => {
-  if (!shouldPromptForUiDisable) {
-    return;
-  }
-
-  shouldPromptForUiDisable = false;
-  const yes = 'Yes';
-  const workspace = 'Only in this Workspace';
-  const no = 'No';
-
-  const result = await vscode.window.showInformationMessage(
-    'Thanks for trying out native VS Code testing! Would you like to disable the default Test Explorer extension UI?',
-    no,
-    yes,
-    workspace
-  );
-
-  if (result === yes || result === workspace) {
-    const target =
-      result === yes ? vscode.ConfigurationTarget.Global : vscode.ConfigurationTarget.Workspace;
-    const config = vscode.workspace.getConfiguration();
-    for (const setting of settings) {
-      config.update(setting, false, target);
-    }
-  }
 };
